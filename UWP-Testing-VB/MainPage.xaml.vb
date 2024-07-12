bebc9a4f6bccef97c8721ed4a45c718324ebe990
AUS Precision <info@ausprecision.com.au>
2024-07-12 21:15:53 +1000

    Added PCB, Rename of Folders, Update of UWP
    Rename of 3D Printing Folders
    Added PCB files (Gerber, Drill)
    
    Update of UWP Testing VB to allow connection to ESP32-CAM MJPEG stream and AUS Precision simulated MJPEG Stream.

--- UWP-Testing-VB/MainPage.xaml.vb
+++ UWP-Testing-VB/MainPage.xaml.vb
@@ -3,6 +3,8 @@
 Imports MjpegProcessor
 
 Imports Windows.Storage.Streams
+Imports Windows.Devices.WiFi
+Imports Windows.Devices
 
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
@@ -10,10 +12,30 @@ Imports Windows.Storage.Streams
 Public NotInheritable Class MainPage
     Inherits Page
 
+    Public fpstimer As DispatcherTimer
+    Public fps As Integer = 0
+
+    Public wiFiAdapters As WiFiAdapter
+
+
+
+
+
+
+
     Private mjpeg As MjpegDecoder = New MjpegDecoder
 
     Public Sub New()
+
+
+
         InitializeComponent()
+        fpstimer = New DispatcherTimer
+        fpstimer.Interval = TimeSpan.FromMilliseconds(5000)
+        AddHandler fpstimer.Tick, AddressOf fpstimer_Tick
+
+        GetWiFiAdapter()
+
 
     End Sub
 
@@ -31,18 +53,56 @@ Public NotInheritable Class MainPage
             stream.Seek(0)
             Await bitmapImage.SetSourceAsync(stream)
         End Using
-
+        Label1.Text = "MJPEG Stream Resolution: " & bitmapImage.PixelWidth.ToString & "x" & bitmapImage.PixelHeight.ToString
+        fps = fps + 1
         ImageBox.Source = bitmapImage
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As RoutedEventArgs) Handles btnStart.Click
         mjpeg.ParseStream(New Uri("http://192.168.4.1:81/stream"))
         AddHandler mjpeg.FrameReady, AddressOf mjpeg_handleframe
+        fpstimer.Start()
     End Sub
 
     Private Sub btnStop_Click(sender As Object, e As RoutedEventArgs) Handles btnStop.Click
         mjpeg.StopStream()
+        fpstimer.Stop()
         RemoveHandler mjpeg.FrameReady, AddressOf mjpeg_handleframe
         mjpeg = Nothing
     End Sub
+
+    Private Sub btnStart_ap_Click(sender As Object, e As RoutedEventArgs) Handles btnStart_ap.Click
+        mjpeg.ParseStream(New Uri("http://stream.ausprecision.com.au:8080"))
+        AddHandler mjpeg.FrameReady, AddressOf mjpeg_handleframe
+        fpstimer.Start()
+    End Sub
+
+    Private Sub fpstimer_Tick(ByVal sender As Object, ByVal e As Object)
+        Label2.Text = "Frames Per Second (FPS): " & Convert.ToDecimal(fps / 5)
+        fps = 0
+        GetWiFiAdapter()
+
+    End Sub
+
+
+    Public Async Sub GetWiFiAdapter()
+        Dim firstAdapter As WiFiAdapter
+        Dim result = Await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector())
+        If result.Count >= 1 Then
+            firstAdapter = Await WiFiAdapter.FromIdAsync(result(0).Id)
+            Await firstAdapter.ScanAsync()
+            Dim wifis = firstAdapter.NetworkReport.AvailableNetworks
+
+
+
+            For Each item In wifis
+                If item.Ssid = "Enclypse" Then
+                    Debug.Write("WiFi Percentage = " & (Math.Min(Math.Max(2 * (item.NetworkRssiInDecibelMilliwatts + 100), 0), 100)).ToString)
+                    Debug.WriteLine(String.Format(" | {0}dBm", item.NetworkRssiInDecibelMilliwatts))
+                End If
+            Next
+
+        End If
+    End Sub
+
 End Class
