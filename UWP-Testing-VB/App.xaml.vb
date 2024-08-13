9762ceb8c24822987e185b6b43a6d2da4c7e499d
AUS Precision <info@ausprecision.com.au>
2024-08-14 08:35:39 +1000

    Visual Basic Code - Beta Release
    Added support for UDP sending and receiving of messages. Camera configuration commands can be sent via an input box. UDP processing of commands will change camera settings on the fly. Basic camera status information is received and displayed.

--- UWP-Testing-VB/App.xaml.vb
+++ UWP-Testing-VB/App.xaml.vb
@@ -1,8 +1,31 @@
-﻿''' <summary>
+﻿Imports System.Net
+Imports System.Net.NetworkInformation
+
+Imports System.Net.Sockets
+Imports System.Text
+Imports Windows.Devices.WiFi
+
+''' <summary>
 ''' Provides application-specific behavior to supplement the default Application class.
 ''' </summary>
 NotInheritable Class App
     Inherits Application
+
+    Public Shared UDPClient As UdpClient = New UdpClient()
+    Public Shared UDPReceived As Boolean = False
+    Public UDPThreadUDPReceive As System.Threading.Thread
+
+    Public BoolExceptionLoop As Boolean = False
+    Public Shared UDPRecievedMessage As String = String.Empty
+    Public ipRemoteEndPoint As IPEndPoint = New IPEndPoint(IPAddress.Any, 11000)
+
+    Public ConnectionWiFiTimer As DispatcherTimer
+    Public Shared ConnectionWiFiConnected As Boolean = False
+    Public Shared ConnectionBaseStationConnected As Boolean = False
+    Public wirelessAdapter As WiFiAdapter
+    Public wirelessNetworkName As String = ""
+
+    Public Shared szStatus_Wifi As String = "[WiFi] Initializing..."
 
     ''' <summary>
     ''' Invoked when the application is launched normally by the end user.  Other entry points
@@ -10,12 +33,10 @@ NotInheritable Class App
     ''' search results, and so forth.
     ''' </summary>
     ''' <param name="e">Details about the launch request and process.</param>
-    Protected Overrides Sub OnLaunched(e As Windows.ApplicationModel.Activation.LaunchActivatedEventArgs)
+    Protected Overrides Async Sub OnLaunched(e As Windows.ApplicationModel.Activation.LaunchActivatedEventArgs)
         Dim rootFrame As Frame = TryCast(Window.Current.Content, Frame)
-
         ' Do not repeat app initialization when the Window already has content,
         ' just ensure that the window is active
-
         If rootFrame Is Nothing Then
             ' Create a Frame to act as the navigation context and navigate to the first page
             rootFrame = New Frame()
@@ -28,7 +49,6 @@ NotInheritable Class App
             ' Place the frame in the current Window
             Window.Current.Content = rootFrame
         End If
-
         If e.PrelaunchActivated = False Then
             If rootFrame.Content Is Nothing Then
                 ' When the navigation stack isn't restored navigate to the first page,
@@ -40,6 +60,20 @@ NotInheritable Class App
             ' Ensure the current window is active
             Window.Current.Activate()
         End If
+
+        UDPClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, True)
+        UDPClient.Client.Bind(New IPEndPoint(IPAddress.Any, 2090))
+        UDPClient.Connect(IPAddress.Parse("192.168.4.1"), 2090)
+
+        UDPThreadUDPReceive = New System.Threading.Thread(AddressOf UDPReceiveMessages)
+        UDPThreadUDPReceive.Start()
+
+
+        ConnectionWiFiTimer = New DispatcherTimer
+        ConnectionWiFiTimer.Interval = TimeSpan.FromMilliseconds(10000)
+        AddHandler ConnectionWiFiTimer.Tick, AddressOf ConnectionWiFiTimer_Tick
+        ConnectionWiFiTimer.Start()
+
     End Sub
 
     ''' <summary>
@@ -62,6 +96,63 @@ NotInheritable Class App
         Dim deferral As SuspendingDeferral = e.SuspendingOperation.GetDeferral()
         ' TODO: Save application state and stop any background activity
         deferral.Complete()
+    End Sub
+
+    Public Async Sub UDPReceiveMessages()
+        Do While BoolExceptionLoop = False
+            Try
+                Dim bytRecieved As Byte() = UDPClient.Receive(ipRemoteEndPoint)
+                UDPRecievedMessage = Encoding.ASCII.GetString(bytRecieved)
+                Debug.WriteLine(UDPRecievedMessage)
+                UDPReceived = True
+            Catch ex As Exception
+                BoolExceptionLoop = True
+                Debug.WriteLine("Error on UDP Listening")
+                Debug.WriteLine(ex.ToString)
+                Debug.WriteLine(ex.Message.ToString)
+            End Try
+            Await Task.Delay(1)
+        Loop
+    End Sub
+
+    Public Shared Sub UDPSendMessages(CommandString As String)
+        Try
+            Dim data As Byte() = Encoding.ASCII.GetBytes(CommandString)
+            UDPClient.Send(data, data.Length)
+        Catch ex As Exception
+            Debug.WriteLine("UDP: Error Sending Delay and Timing Data to Base Station")
+        End Try
+    End Sub
+
+
+
+    Private Async Sub ConnectionWiFiTimer_Tick(ByVal sender As Object, ByVal e As Object)
+        Try
+            Dim result = Await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector())
+            If result.Count >= 1 Then
+                For x = 0 To result.Count - 1
+                    wirelessAdapter = Await WiFiAdapter.FromIdAsync(result(x).Id)
+                    Dim connectedprofile = Await wirelessAdapter.NetworkAdapter.GetConnectedProfileAsync()
+                    If connectedprofile IsNot Nothing Then
+                        wirelessNetworkName = connectedprofile.ProfileName
+                        ConnectionWiFiConnected = True
+                        Debug.WriteLine("[WiFi] SSID: " & wirelessNetworkName & " | " & (Math.Min(Math.Max(2 * (wirelessAdapter.NetworkReport.AvailableNetworks(0).NetworkRssiInDecibelMilliwatts + 100), 0), 100)).ToString & "%" & " (RSSI: " & wirelessAdapter.NetworkReport.AvailableNetworks(0).NetworkRssiInDecibelMilliwatts & "dbm)")
+                        szStatus_Wifi = "[WiFi] SSID: " & wirelessNetworkName & " | " & (Math.Min(Math.Max(2 * (wirelessAdapter.NetworkReport.AvailableNetworks(0).NetworkRssiInDecibelMilliwatts + 100), 0), 100)).ToString & "%" & " (RSSI: " & wirelessAdapter.NetworkReport.AvailableNetworks(0).NetworkRssiInDecibelMilliwatts & "dbm)"
+                        Exit For
+                    Else
+                        ConnectionWiFiConnected = False
+                    End If
+                Next
+            Else
+                Debug.WriteLine("[WiFi] - Not Connected / Local LAN Connection")
+                szStatus_Wifi = "[WiFi] - Not Connected / Local LAN Connection"
+            End If
+        Catch ex As Exception
+            Debug.WriteLine("[WiFi] Error - Failed to Retrieve WiFi Network Information")
+            szStatus_Wifi = "[WiFi] Error - Failed to Retrieve WiFi Network Information"
+            Debug.WriteLine(ex.ToString)
+            Debug.WriteLine(ex.Message.ToString)
+        End Try
     End Sub
 
 End Class
--- UWP-Testing-VB/MainPage.xaml.vb
+++ UWP-Testing-VB/MainPage.xaml.vb
@@ -5,6 +5,10 @@ Imports MjpegProcessor
 Imports Windows.Storage.Streams
 Imports Windows.Devices.WiFi
 Imports Windows.Devices
+Imports System.Net.Sockets
+Imports System.Net
+Imports System.Text
+Imports ManagedNativeWifi
 
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
@@ -13,29 +17,24 @@ Public NotInheritable Class MainPage
     Inherits Page
 
     Public fpstimer As DispatcherTimer
+    Public status_timer As DispatcherTimer
+
     Public fps As Integer = 0
-
-    Public wiFiAdapters As WiFiAdapter
-
-
-
-
-
-
-
     Private mjpeg As MjpegDecoder = New MjpegDecoder
 
+
     Public Sub New()
-
-
-
         InitializeComponent()
         fpstimer = New DispatcherTimer
-        fpstimer.Interval = TimeSpan.FromMilliseconds(5000)
+        fpstimer.Interval = TimeSpan.FromMilliseconds(10000)
         AddHandler fpstimer.Tick, AddressOf fpstimer_Tick
 
-        GetWiFiAdapter()
+        status_timer = New DispatcherTimer
+        status_timer.Interval = TimeSpan.FromMilliseconds(1)
+        AddHandler status_timer.Tick, AddressOf status_timer_Tick
+        status_timer.Start()
 
+        textbox_command.IsSpellCheckEnabled = False
 
     End Sub
 
@@ -80,29 +79,18 @@ Public NotInheritable Class MainPage
     Private Sub fpstimer_Tick(ByVal sender As Object, ByVal e As Object)
         Label2.Text = "Frames Per Second (FPS): " & Convert.ToDecimal(fps / 5)
         fps = 0
-        GetWiFiAdapter()
-
     End Sub
 
-
-    Public Async Sub GetWiFiAdapter()
-        Dim firstAdapter As WiFiAdapter
-        Dim result = Await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(WiFiAdapter.GetDeviceSelector())
-        If result.Count >= 1 Then
-            firstAdapter = Await WiFiAdapter.FromIdAsync(result(0).Id)
-            Await firstAdapter.ScanAsync()
-            Dim wifis = firstAdapter.NetworkReport.AvailableNetworks
-
-
-
-            For Each item In wifis
-                If item.Ssid = "Enclypse" Then
-                    Debug.Write("WiFi Percentage = " & (Math.Min(Math.Max(2 * (item.NetworkRssiInDecibelMilliwatts + 100), 0), 100)).ToString)
-                    Debug.WriteLine(String.Format(" | {0}dBm", item.NetworkRssiInDecibelMilliwatts))
-                End If
-            Next
-
-        End If
+    Private Sub button_setres_Click(sender As Object, e As RoutedEventArgs) Handles button_setres.Click
+        App.UDPSendMessages("camera set res SVGA")
     End Sub
 
+    Private Sub status_timer_Tick(ByVal sender As Object, ByVal e As Object)
+        lbl_status_wifi.Text = App.szStatus_Wifi
+        If App.UDPReceived = True Then lbl_status_camera.Text = "[Camera] " & App.UDPRecievedMessage
+    End Sub
+
+    Private Sub button_send_command_Click(sender As Object, e As RoutedEventArgs) Handles button_send_command.Click
+        App.UDPSendMessages(textbox_command.Text)
+    End Sub
 End Class
